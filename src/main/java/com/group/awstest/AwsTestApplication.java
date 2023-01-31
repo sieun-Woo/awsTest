1b01bccfd7bdb45963ab320972b6e44c5f0a1f36
sieun-Woo <116184724+sieun-Woo@users.noreply.github.com>
2023-01-31 10:28:43 +0900

    Initial commit

---
+++ src/main/java/com/group/awstest/AwsTestApplication.java
@@ -0,0 +1,13 @@
+package com.group.awstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsTestApplication {
+
+    //test
+    public static void main(String[] args) {
+        SpringApplication.run(AwsTestApplication.class, args);
+    }
+}
