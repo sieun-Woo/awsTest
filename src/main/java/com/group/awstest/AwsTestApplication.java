6fc36475df81cd8dfd7c9624bb6d47391104b928
sieun-Woo <116184724+sieun-Woo@users.noreply.github.com>
2023-01-31 15:14:57 +0900

    update

--- src/main/java/com/group/awstest/AwsTestApplication.java
+++ src/main/java/com/group/awstest/AwsTestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AwsTestApplication {
 
     //test
+    
     public static void main(String[] args) {
         SpringApplication.run(AwsTestApplication.class, args);
     }
